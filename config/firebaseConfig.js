b418fa9170186f012ba94be5c37d7e2313920ec2
Naitik Madarwal <naitikmadarwal3806@gmail.com>
2025-09-24 15:01:10 +0530

    Firebase Initialization

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,8 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,6 +12,8 @@ const firebaseConfig = {
     appId: "1:730452334111:web:ee28c9c2905cd709b5e72a"
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+// Initialize Firebase only if it hasn't been initialized yet
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+
+export { app, db };
